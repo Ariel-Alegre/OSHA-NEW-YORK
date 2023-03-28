10a9747858de05f732568894907993cb6d9b90e6
Ariel-Alegre <arielalegre98@gmail.com>
2023-03-28 11:59:14 -0300

    enviroment

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -2,27 +2,27 @@ require('dotenv').config()
 
 module.exports ={
   development: {
-    username: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "root",
+    database: "railway",
+    password: "nmbBkCYMXQ0oDwS4jTKa",
+    host: "containers-us-west-145.railway.app",
+    port: "7798",
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "root",
+    database: "railway",
+    password: "nmbBkCYMXQ0oDwS4jTKa",
+    host: "containers-us-west-145.railway.app",
+    port: "7798",
     dialect: "mysql"
   },
   production: {
     username: "root",
     database: "railway",
-    password: "j37EP1eaeypz7btfcEmw",
-    host: "containers-us-west-104.railway.app",
-    port: "5654",
+    password: "nmbBkCYMXQ0oDwS4jTKa",
+    host: "containers-us-west-145.railway.app",
+    port: "7798",
     dialect: "mysql"
 
   }
