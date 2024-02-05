2f5f51e0efcdac186beca32ad5a549211819e666
Ariel-Alegre <arielalegre98@gmail.com>
2024-02-05 19:14:34 -0300

    redeploy

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -4,26 +4,26 @@ module.exports = {
   development: {
  username: "root",
     database: "railway",
-    password: "uuyxtE4Tk3LZQOsVf5Qg",
-    host: "containers-us-west-145.railway.app",
-    port: "7798",
+    password: "GcFB6BEcffdFeB6112Ce5F5hA3e5E3AD",
+    host: "roundhouse.proxy.rlwy.net",
+    port: "22864",
     dialect: "mysql" 
   },
    test: {
     username: "root",
     database: "railway",
-    password: "uuyxtE4Tk3LZQOsVf5Qg",
-    host: "containers-us-west-145.railway.app",
-    port: "7798",
-    dialect: "mysql"
+    password: "GcFB6BEcffdFeB6112Ce5F5hA3e5E3AD",
+    host: "roundhouse.proxy.rlwy.net",
+    port: "22864",
+    dialect: "mysql" 
   },
   production: {
     username: "root",
     database: "railway",
-    password: "uuyxtE4Tk3LZQOsVf5Qg",
-    host: "containers-us-west-145.railway.app",
-    port: "7798",
-    dialect: "mysql"
+    password: "GcFB6BEcffdFeB6112Ce5F5hA3e5E3AD",
+    host: "roundhouse.proxy.rlwy.net",
+    port: "22864",
+    dialect: "mysql" 
 
   }  
  /*  development: {
